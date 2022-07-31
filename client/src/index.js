05eabe97c816b3a1f36b200a35852cdae7042679
CleoBrown <gillmoynagh@hotmail.co.uk>
2022-07-31 10:07:34 +0100

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'leaflet/dist/leaflet.css';
+
+
+<script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"
+  integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ=="
+  crossorigin=""></script>
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const db_hostname = process.env.DB_HOSTNAME || "localhost";
+
+app.use(express.json());
+app.use(cors());
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect(`mongodb://${db_hostname}:27017/`, { useUnifiedTopology: true })
+    .then(console.log(`Connected to mongodb://${db_hostname}:27017/`))
+    .then((client) => {
+        const db = client.db('countriesApp');
+        const countriesCollection = db.collection('countries');
+        const countriesRouter = createRouter(countriesCollection);
+        app.use('/api/countries', countriesRouter);
+
+        const usersCollection = db.collection('users');
+        const usersRouter = createRouter(usersCollection);
+        app.use('/api/users', usersRouter);
+    })
+    .catch(console.error)
+
+app.listen(9000, function () {
+    console.log(`Listening on port ${this.address().port}`);
+});
